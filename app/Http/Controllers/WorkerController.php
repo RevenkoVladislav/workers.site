04118fdceee680ba9486b827e9e022627ff54639
RevenkoVladislav <Vlad_reven@mail.ru>
2025-11-27 21:24:14 +0600

    Add seeder for Workers

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -10,7 +10,7 @@ class WorkerController extends Controller
 {
     public function index()
     {
-        $workers = Worker::all();
+        $workers = Worker::paginate(6);
         return view('worker.index', compact('workers'));
     }
 
--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Worker extends Model
 {
+    use HasFactory;
     protected $table = 'workers';
 
     protected $fillable = [
