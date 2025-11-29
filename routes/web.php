75c1735c1a9a4be0bc202a2446578912b4064399
RevenkoVladislav <Vlad_reven@mail.ru>
2025-11-29 22:11:11 +0600

    install laravel ui

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\WorkerController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('workers', WorkerController::class);
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
