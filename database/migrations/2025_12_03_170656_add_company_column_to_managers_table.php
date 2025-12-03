6c09bab97a4fb4b9cd9598363e9f97b4be90e4e6
RevenkoVladislav <Vlad_reven@mail.ru>
2025-12-03 23:09:38 +0600

    Add column to managres tabl

---
+++ database/migrations/2025_12_03_170656_add_company_column_to_managers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('managers', function (Blueprint $table) {
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->foreign('company_id', 'company_id_fk')->references('id')->on('companies')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('managers', function (Blueprint $table) {
+            $table->dropForeign('company_id_fk');
+            $table->dropColumn('company_id');
+        });
+    }
+};
