3afc8511a34ab4c02c8d46a4746073f76a1e9ae1
RevenkoVladislav <Vlad_reven@mail.ru>
2025-12-04 21:53:49 +0600

    Add ManagerFactory and CompanyFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
+use App\Models\Manager;
 use App\Models\Role;
 use App\Models\User;
 use App\Models\Worker;
@@ -17,9 +19,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)
+        User::factory(20)
             ->for(Role::where('name', 'Worker')->first())
             ->has(Worker::factory())
+            ->create();
+
+        Company::factory(5)->create();
+
+        User::factory(10)
+            ->for(Role::where('name', 'Manager')->first())
+            ->has(Manager::factory())
             ->create();
     }
 }
